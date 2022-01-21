70f00a738b4ef6493986711f646085aaf1f72c37
marcin <kto tam@>
2022-01-21 16:25:12 +0100

    sdsadas

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,7 +8,7 @@ function Contact(props) {
     const stan=useSelector(state=>state.height.height)
     const {height}=bindActionCreators(actionCreator,dispatch)
     useEffect(()=>{
-        // props.give(false)
+        props.give(false)
         
         // height(window.innerHeight)
         // props.send.b(!props.send.a)
@@ -26,7 +26,7 @@ function Contact(props) {
           <br/>
           
           <h1 className="h1email">...or send me e-mail</h1>
-          <div className="byemail">
+
             
           <form  action="https://formsubmit.co/kalwara_marcin@o2.pl" method="POST" >
               <label htmlFor="email" className="label">Enter your e-mail</label>
@@ -38,7 +38,7 @@ function Contact(props) {
               <br/>
               <button name="email" type="submit" className="send">send</button>
           </form>
-          </div>
+    
                 </div>
     )
 }
